5e990cca65819b187f46d7f5be39498bb51d329a
TommiRaisanen <t3rato01@students.oamk>
2025-01-24 11:38:09 +0200

    teht3 (vahingossa nimesin teht4)

--- Teht3/main.cpp
+++ Teht3/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include "kokki.h"
+#include "italianchef.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    Chef kokki_olio("Gordon");
+
+    int sAnnoksia = kokki_olio.makeSalad(50);
+        cout << "annoksia : ";
+         cout << sAnnoksia
+                << endl;
+
+    int kAnnoksia = kokki_olio.makeSoup(15);
+         cout << "annoksia : ";
+         cout << kAnnoksia
+         << endl;
+
+
+         ItalianChef i_olio("mario");
+        i_olio.askSecret("pizza", 50, 50);
+
+
+
+
     return 0;
 }
